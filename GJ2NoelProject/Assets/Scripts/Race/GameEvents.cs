affd7cf8fbed709348a5fc7576c727babcdd6aab
Christophe Fracheboud <christophe.fracheboud.rime@gmail.com>
2022-12-21 10:02:25 +0100

    Count down everywhere

--- GJ2NoelProject/Assets/Scripts/Race/GameEvents.cs
+++ GJ2NoelProject/Assets/Scripts/Race/GameEvents.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public static class GameEvents
 {
-    public delegate void StartRace();
+    /*public delegate void StartRace();
     public static StartRace OnStartRace;
 
     public delegate void WaitForPlayers();
-    public static WaitForPlayers OnWaitForPlayers;
+    public static WaitForPlayers OnWaitForPlayers;*/
+
+    public delegate void GameEvent();
+
+    public static GameEvent OnStartRace;
+    public static GameEvent OnWaitForPlayers;
 }
--- GJ2NoelProject/Assets/Scripts/Race/StartRaceManager.cs
+++ GJ2NoelProject/Assets/Scripts/Race/StartRaceManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 using TMPro;
+using System;
 
 public class StartRaceManager : NetworkBehaviour
 {
@@ -15,17 +16,18 @@ public class StartRaceManager : NetworkBehaviour
 
     private void Start()
     {
-        if (!IsServer) return;
+        _countDownText.text = "Waiting for all player to be connected ...";
 
+        if (!IsServer) return;
         StartCoroutine(CountDownCoroutine());
     }
 
     private IEnumerator CountDownCoroutine()
     {
         CountDownClientRpc("Waiting for all player to be connected ...");
-        GameEvents.OnWaitForPlayers?.Invoke();
+        InvokeWaitForPlayerEventsClientRpc();
 
-        // Should wait for all to be connected, but now it's in time
+        // Should wait for all to be connected, but for now it's in time
         yield return new WaitForSeconds(_timeBeforeCountdown);
 
         for (int i = _countDownMax; i > 0; i--)
@@ -35,18 +37,27 @@ public class StartRaceManager : NetworkBehaviour
         }
 
         CountDownClientRpc("Start !!!");
-
-        GameEvents.OnStartRace?.Invoke();
+        InvokeStartRaceEventsClientRpc();
 
         yield return new WaitForSeconds(1);
         CountDownClientRpc("");
     }
 
     [ClientRpc]
+    private void InvokeWaitForPlayerEventsClientRpc()
+    {
+        GameEvents.OnWaitForPlayers?.Invoke();
+    }
+
+    [ClientRpc]
+    private void InvokeStartRaceEventsClientRpc()
+    {
+        GameEvents.OnStartRace?.Invoke();
+    }
+
+    [ClientRpc]
     private void CountDownClientRpc(string value)
     {
-        if (!IsOwner) return;
-
         // Change the countdown text
         _countDownText.text = value;
     }
